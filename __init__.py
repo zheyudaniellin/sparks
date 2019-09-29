bd9801c2daca99a1ee1c301085e2baf822a78e1f
zydLin <zdl3gk@virginia.edu>
2019-09-29 12:18:17 -0400

    initial commit

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+    SPARKS: Stacking Spectra for Disk Structures
+    main author: Zhe-Yu Daniel Lin
+    
+"""
+
+__version__ = "0.01"
+__author__ = 'Zhe-Yu Daniel Lin'
+__copyright__ = 'Copyright (C) 2019 Zhe-Yu Daniel Lin'
---
+++ natconst.py
@@ -0,0 +1,16 @@
+# natconst.py
+# place to keep natural constants in cgs units
+
+G = 6.6272e-8   # Gravitational constant
+mp = 1.6726e-24 # mass of proton
+k = 1.3807e-16  # Boltzmann's constant
+sb = 5.6703e-5  # Stefan-Boltzmann constant
+mu = 2.3        # mean molecular weight
+Jy = 1e-23      # Jansky
+Lsun = 3.8525e33        # Solar luminosity
+Rsun = 6.96e10  # Solar radius
+Msun = 1.99e33  # solar mass
+Tsun = 5780.0   # temperature of Sun
+AU = 1.496e13   # astronomical unit
+pc = 3.08572e18 # parsec
+rad = 0.017453292519943295	# pi / 180
